eb03d085b0b30cbbc08443210b65588368649537
Slimoftheshady <samuelou6009@gmail.com>
2024-03-04 22:32:57 +0800

    removed function

--- cesar.py
+++ cesar.py
@@ -1,19 +1,5 @@
 count = 0
 
-def CheckSpecialChr(i):
-    if i == 32:
-        return i
-    elif i == 33:
-        return i
-    elif i == 44:
-        return i
-    elif i == 46:
-        return i
-    elif i == 63:
-        return i
-    else:
-        return i
-
 encrypt = input("Input Text: ")
 list1 = list(encrypt)
 print(list1)
@@ -26,8 +12,18 @@ print(list2)
 
 while count != 26:
     for i in range(len(list2)):
-        list2[i] = CheckSpecialChr(list2[i])
-        if list2[i] == 90:
+        if list2[i] == 32:  # Check for space
+            continue  # Skip increment for space
+        if list2[i] == 33: #these lines check for special characters (like space, question mark)
+            continue  
+        if list2[i] == 44:
+            continue  
+        if list2[i] == 46:
+            continue  
+        if list2[i] == 63:
+            continue  
+
+        elif list2[i] == 90:
             list2[i] = 65
         elif list2[i] == 122:
             list2[i] = 97
