5f2f9b6212bf37a25899fef2fc10f3cd65f1cf46
Slimoftheshady <samuelou6009@gmail.com>
2024-03-04 22:37:33 +0800

    removed test prints, fin

--- cesar.py
+++ cesar.py
@@ -1,34 +1,25 @@
-count = 0
+count = 1
 
 encrypt = input("Input Text: ")
 list1 = list(encrypt)
-print(list1)
+
 
 list2 = []
 for i in list1:
     list2.extend(ord(char) for char in i)
 
-print(list2)
-
 while count != 26:
     for i in range(len(list2)):
-        if list2[i] == 32:  # Check for space
-            continue  # Skip increment for space
-        if list2[i] == 33: #these lines check for special characters (like space, question mark)
-            continue  
-        if list2[i] == 44:
-            continue  
-        if list2[i] == 46:
-            continue  
-        if list2[i] == 63:
-            continue  
+        if list2[i] == 32 or list2[i] in [33, 44, 46, 63]:  #Checks for special characters e.g space, question mark
+            continue #runs through loop, only works in a loop
 
-        elif list2[i] == 90:
+        if list2[i] == 90:
             list2[i] = 65
         elif list2[i] == 122:
             list2[i] = 97
         else:
             list2[i] += 1
 
-    print(list2)
+    text_list = [chr(val) for val in list2]
+    print("Letters Plus " + str(count) + ": ", ''.join(text_list))
     count += 1
