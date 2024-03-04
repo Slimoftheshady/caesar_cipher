e544a27d7bc27d50b163fd277ca868f5bf456a44
Slimoftheshady <samuelou6009@gmail.com>
2024-03-04 22:25:46 +0800

    cleaned up, function not work

--- cesar.py
+++ cesar.py
@@ -1,8 +1,38 @@
-encrypt = input("Input Text: ")
-list = list(encrypt)
-print(list)
+count = 0
 
-res = []
-for i in list:
-    res.extend(ord(num) for num in i)
-print(res)
+def CheckSpecialChr(i):
+    if i == 32:
+        return i
+    elif i == 33:
+        return i
+    elif i == 44:
+        return i
+    elif i == 46:
+        return i
+    elif i == 63:
+        return i
+    else:
+        return i
+
+encrypt = input("Input Text: ")
+list1 = list(encrypt)
+print(list1)
+
+list2 = []
+for i in list1:
+    list2.extend(ord(char) for char in i)
+
+print(list2)
+
+while count != 26:
+    for i in range(len(list2)):
+        list2[i] = CheckSpecialChr(list2[i])
+        if list2[i] == 90:
+            list2[i] = 65
+        elif list2[i] == 122:
+            list2[i] = 97
+        else:
+            list2[i] += 1
+
+    print(list2)
+    count += 1
